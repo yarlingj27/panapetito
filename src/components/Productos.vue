cba212ebfee38ba14f7a8e0713122bd169bb27c6
yarlingj27 <yarlingj27@gmail.com>
2024-06-23 16:27:34 -0500

    Estilos css al componente de productos

--- src/components/Productos.vue
+++ src/components/Productos.vue
@@ -37,3 +37,48 @@ watchEffect(() => {
 })
 </script>
 
+<style escoped>
+.contenedor{
+    padding: 3rem;
+    margin-bottom: 2rem;
+}
+.productos{
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 2rem;
+}
+.producto{
+    display: flex;
+    align-items: center;
+}
+.informacion{
+    padding: 15px;
+}
+.titulo {
+    font-size: 1.25rem;
+}
+.descripcion {
+    color: gray;
+    word-wrap: break-word;
+}
+.foto {
+    max-width: calc(100%/4);
+    border-radius: 20px;
+    box-shadow: 0px 2px 4px rgb(76, 76, 76);
+}
+.precio {
+    color: #f9a72b;
+    font-weight: bold;
+    font-size: 1.25rem;
+}
+@media (max-width: 1024px) and (min-width: 768px){
+.productos {
+    grid-template-columns: repeat(2, 1fr);
+}
+}
+@media(max-width: 768px){
+    .productos {
+        grid-template-columns: repeat(1, 1fr);
+    }
+}
+</style>
