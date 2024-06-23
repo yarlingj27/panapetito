3041339a3853639e363d175be3ff34ff478e1338
yarlingj27 <yarlingj27@gmail.com>
2024-06-23 17:13:00 -0500

    Pantalla de Autor Creada y terminada

---
+++ src/views/AutorView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="contenedor">
+        <div class="autor">
+            <img src="/imagenes/systemplus.png" alt="">
+            <p>Este proyecto fue desarrollado por:</p>
+            <h3>Yarlin Gómez</h3>
+            <p>Curso Diseño Web - systemplus - 2024</p>
+            <a href="https://github.com/yarlingj27/panapetito" target="_blank">yarlingj27/panapetito</a>
+        </div>
+    </div>
+</template>
+<style scoped>
+.contenedor{
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+}
+img{
+    max-width: 50%;
+}
+a{
+    text-decoration: none;
+    color: inherit;
+}
+</style>
